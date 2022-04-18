0f2479d9cad94d379fc64b68c292f5d9f931f426
inwoo2ya <86223023+inwoo2ya@users.noreply.github.com>
2022-04-19 02:06:50 +0900

    Create 약수의개수 덧셈.py

---
+++ 약수의개수 덧셈.py
@@ -0,0 +1,14 @@
+def solution(left, right):
+    answer = 0
+    for n in range(left,right+1):
+        nu = 0
+        for i in range(1,n+1):
+            if n%i == 0 :
+                nu+=1
+
+        if nu%2 ==0 :
+            answer += n
+        else :
+            answer -= n
+
+    return answer
